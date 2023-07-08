7acdc5081f848d4c40a7a4424141eddf7b44f1e2
marsel-asanbek-0ff <bajdalievmarsel@gmail.com>
2023-07-08 16:40:13 +0600

    ex-4

---
+++ main.js
@@ -0,0 +1,11 @@
+var subscription = prompt('Выберите тип подписки (vip , free , pro)')
+
+    if (subscription == 'pro' || subscription == 'vip') {
+      alert ('Доступ разрешен. Пользователь имеет подписку pro или vip.');
+    }
+     else {
+      alert ("Доступ запрещен. Пользователь должен иметь подписку pro или vip.");
+     }
+    
+  
+
